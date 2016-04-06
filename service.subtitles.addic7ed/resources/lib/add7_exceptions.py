6c8f5bb6a725d0fe552c60c21279aac95da6377e
Roman Miroshnychenko <romanvm@yandex.ua>
2016-04-06 13:15:11 +0300

    Refactors functions.py

---
+++ service.subtitles.addic7ed/resources/lib/add7_exceptions.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# Created on: 06.04.2016
+# Author: Roman Miroshnychenko aka Roman V.M. (romanvm@yandex.ua)
+
+
+class Add7Exception(Exception):
+    pass
+
+
+class ParseError(Add7Exception):
+    pass
