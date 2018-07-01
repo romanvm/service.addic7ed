0e8655a3159950c666f906b3b52b8ec78acc39af
Roman Miroshnychenko <roman1972@gmail.com>
2018-07-01 10:30:25 +0300

    Add addon module for common addon parameters

---
+++ service.subtitles.addic7ed/addic7ed/addon.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+import os
+import xbmc
+from xbmcaddon import Addon
+
+__all__ = ['ADDON_ID', 'addon', 'path', 'profile', 'icon', 'get_ui_string']
+
+ADDON_ID = 'script.service.addic7ed'
+addon = Addon(ADDON_ID)
+path = xbmc.translatePath(addon.getAddonInfo('path')).decode('utf-8')
+profile = xbmc.translatePath(addon.getAddonInfo('profile')).decode('utf-8')
+icon = os.path.join(path, 'icon.png')
+
+
+def get_ui_string(string_id):
+    """
+    Get language string by ID
+
+    :param string_id: UI string ID
+    :return: UI string
+    """
+    return addon.getLocalizedString(string_id)
