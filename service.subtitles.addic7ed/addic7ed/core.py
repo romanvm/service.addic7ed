520062e87d9dde532ebba2a99e9657cdf2c24fe6
Roman Miroshnychenko <roman1972@gmail.com>
2018-07-01 10:30:58 +0300

    Refactor core module

--- service.subtitles.addic7ed/addic7ed/core.py
+++ service.subtitles.addic7ed/addic7ed/core.py
@@ -12,48 +12,24 @@ import urllib
 import shutil
 #XBMC modules
 import xbmc
-import xbmcaddon
 import xbmcplugin
 import xbmcgui
 import xbmcvfs
 
-from . import parser, functions
+from . import parser
+from .addon import addon, get_ui_string, icon
 from .exceptions import *
+from .functions import (log_notice, log_error, log_debug, get_languages,
+                        get_now_played, parse_filename, normalize_showname)
 
 __all__ = ['router']
 
-_addon = xbmcaddon.Addon()
-_id = _addon.getAddonInfo('id')
-_path = _addon.getAddonInfo('path').decode('utf-8')
-_profile = xbmc.translatePath(_addon.getAddonInfo('profile')).decode('utf-8')
-_temp = os.path.join(_profile, 'temp')
-_handle = int(sys.argv[1])
-_icon = os.path.join(_path, 'icon.png')
+temp = os.path.join(addon.profile, 'temp')
+handle = int(sys.argv[1])
 
 
 VIDEOFILES = ('.avi', '.mkv', '.mp4', '.ts', '.m2ts', '.mov')
 dialog = xbmcgui.Dialog()
-
-
-def log(message, level=xbmc.LOGDEBUG):
-    """
-    Write message to the Kodi log
-    for debuging purposes.
-    """
-    if isinstance(message, unicode):
-        message = message.encode('utf-8')
-    xbmc.log('{0}: {1}'.format(_id, message), level)
-
-
-def ui_string(string_id):
-    """
-    Get language string by ID
-
-    :param string_id: UI string ID
-    :return: UI string
-    """
-    return _addon.getLocalizedString(string_id)
-
 
 def display_subs(subs_list, episode_url, filename):
     """
@@ -80,21 +56,30 @@ def display_subs(subs_list, episode_url, filename):
         - url: a plugin call-back URL for downloading selected subs.
     """
     for item in subs_list:
-        list_item = xbmcgui.ListItem(label=item.language,
-                                     label2=item.version.encode('utf-8'),
-                                     thumbnailImage=xbmc.convertLanguage(item.language, xbmc.ISO_639_1))
+        list_item = xbmcgui.ListItem(
+            label=item.language,
+            label2=item.version.encode('utf-8'),
+            thumbnailImage=xbmc.convertLanguage(item.language, xbmc.ISO_639_1)
+        )
         if item.hi:
             list_item.setProperty('hearing_imp', 'true')
         # Check the release name in the filename (e.g. DIMENSION)
         # and compare it with the subs description on addic7ed.com.
-        release_match = re.search(r'\-(.*?)\.', filename)
+        release_match = re.search(r'-(.*?)\.', filename)
         if release_match is not None and release_match.group(1).lower() in item.version.lower():
             # Set 'sync' = 'true' if the subs for the same release.
             list_item.setProperty('sync', 'true')
         url = '{0}?{1}'.format(
             sys.argv[0],
-            urllib.urlencode({'action': 'download', 'link': item.link, 'ref': episode_url, 'filename': filename}))
-        xbmcplugin.addDirectoryItem(handle=_handle, url=url, listitem=list_item, isFolder=False)
+            urllib.urlencode(
+                {'action': 'download',
+                 'link': item.link,
+                 'ref': episode_url,
+                 'filename': filename}
+            )
+        )
+        xbmcplugin.addDirectoryItem(handle=handle, url=url, listitem=list_item,
+                                    isFolder=False)
 
 
 def download_subs(link, referrer, filename):
@@ -110,20 +95,21 @@ def download_subs(link, referrer, filename):
         label - the download location for subs.
     """
     # Re-create a download location in a temporary folder
-    if xbmcvfs.exists(_temp):
-        shutil.rmtree(_temp)
-    xbmcvfs.mkdirs(_temp)
+    if xbmcvfs.exists(temp):
+        shutil.rmtree(temp)
+    xbmcvfs.mkdirs(temp)
     # Combine a path where to download the subs
-    subspath = os.path.join(_temp, filename[:-3] + 'srt')
+    subspath = os.path.join(temp, filename[:-3] + 'srt')
     # Download the subs from addic7ed.com
     try:
         parser.download_subs(link, referrer, subspath)
     except ConnectionError:
-        log('Unable to connect to addic7ed.com', xbmc.LOGERROR)
-        dialog.notification(ui_string(32002), ui_string(32005), 'error')
+        log_error('Unable to connect to addic7ed.com')
+        dialog.notification(get_ui_string(32002), get_ui_string(32005), 'error')
     except DailyLimitError:
-        dialog.notification(ui_string(32002), ui_string(32003), 'error', 3000)
-        log('Exceeded daily limit for subs downloads.', xbmc.LOGERROR)
+        dialog.notification(get_ui_string(32002), get_ui_string(32003), 'error',
+                            3000)
+        log_error('Exceeded daily limit for subs downloads.')
     else:
         # Create a ListItem for downloaded subs and pass it
         # to the Kodi subtitles engine to move the downloaded subs file
@@ -132,32 +118,35 @@ def download_subs(link, referrer, filename):
         # in 'Settings > Video > Subtitles' section.
         # A 2-letter language code will be added to subs filename.
         list_item = xbmcgui.ListItem(label=subspath)
-        xbmcplugin.addDirectoryItem(handle=_handle, url=subspath, listitem=list_item, isFolder=False)
-        dialog.notification(ui_string(32000), ui_string(32001), _icon, 3000, False)
-        log('Subs downloaded.')
+        xbmcplugin.addDirectoryItem(handle=handle, url=subspath, listitem=list_item,
+                                    isFolder=False)
+        dialog.notification(get_ui_string(32000), get_ui_string(32001), icon,
+                            3000, False)
+        log_notice('Subs downloaded.')
 
 
 def search_subs(params):
-    log('Searching for subs...', xbmc.LOGNOTICE)
-    languages = functions.get_languages(urllib.unquote_plus(params['languages']).split(','))
-    now_played = functions.get_now_played()
+    log_notice('Searching for subs...')
+    languages = get_languages(urllib.unquote_plus(params['languages']).split(','))
+    now_played = get_now_played()
     filename = os.path.basename(urllib.unquote(now_played['file']))
-    if _addon.getSetting('use_filename') == 'true' or not now_played['showtitle']:
+    if addon.getSetting('use_filename') == 'true' or not now_played['showtitle']:
         # Try to get showname/season/episode data from
         # the filename if 'use_filename' setting is true
         # or if the video-file does not have library metadata.
         try:
-            log('Using filename: {0}'.format(filename))
-            showname, season, episode = functions.parse_filename(filename)
+            log_debug('Using filename: {0}'.format(filename))
+            showname, season, episode = parse_filename(filename)
         except ParseError:
-            log('Filename {0} failed. Trying ListItem.Label...'.format(filename))
+            log_debug('Filename {0} failed. Trying ListItem.Label...'.format(filename))
             try:
                 filename = now_played['label']
-                log('Using filename: {0}'.format(filename))
-                showname, season, episode = functions.parse_filename(filename)
+                log_debug('Using filename: {0}'.format(filename))
+                showname, season, episode = parse_filename(filename)
             except ParseError:
-                log('Unable to determine episode data for {0}'.format(filename), xbmc.LOGERROR)
-                dialog.notification(ui_string(32002), ui_string(32006), 'error', 3000)
+                log_error('Unable to determine episode data for {0}'.format(filename))
+                dialog.notification(get_ui_string(32002), get_ui_string(32006),
+                                    'error', 3000)
                 return
     else:
         # Get get showname/season/episode data from
@@ -168,47 +157,48 @@ def search_subs(params):
         episode = str(now_played['episode']).zfill(2)
         if not os.path.splitext(filename)[1].lower() in VIDEOFILES:
             filename = '{0}.{1}x{2}.foo'.format(showname.encode('utf-8'), season, episode)
-        log('Using library metadata: {0} - {1}x{2}'.format(showname.encode('utf-8'), season, episode))
+        log_debug('Using library metadata: {0} - {1}x{2}'.format(
+            showname.encode('utf-8'), season, episode)
+        )
     # Search subtitles in Addic7ed.com.
     if params['action'] == 'search':
         # Create a search query string
-        query = '{0}+{1}x{2}'.format(
-            urllib.quote_plus(
-                functions.normalize_showname(showname).encode('utf-8')
-            ),
-            season, episode
+        query = '{0} {1}x{2}'.format(
+            normalize_showname(showname).encode('utf-8'),
+            season,
+            episode
         )
     else:
         # Get the query string typed on the on-screen keyboard
         query = params['searchstring']
     if query:
-        log('Search query: {0}'.format(query))
+        log_debug('Search query: {0}'.format(query))
         try:
             results = parser.search_episode(query, languages)
         except ConnectionError:
-            log('Unable to connect to addic7ed.com', xbmc.LOGERROR)
-            dialog.notification(ui_string(32002), ui_string(32005), 'error')
+            log_error('Unable to connect to addic7ed.com')
+            dialog.notification(get_ui_string(32002), get_ui_string(32005), 'error')
         except SubsSearchError:
-            log('No subs found.', xbmc.LOGNOTICE)
+            log_notice('No subs found.')
         else:
             if isinstance(results, list):
-                log('Multiple episode found:\n{0}'.format(results), xbmc.LOGNOTICE)
-                i = dialog.select(ui_string(32008), [item.title for item in results])
+                log_notice('Multiple episode found:\n{0}'.format(results))
+                i = dialog.select(get_ui_string(32008), [item.title for item in results])
                 if i >= 0:
                     try:
                         results = parser.get_episode(results[i].link, languages)
                     except ConnectionError:
-                        log('Unable to connect to addic7ed.com', xbmc.LOGERROR)
-                        dialog.notification(ui_string(32002), ui_string(32005),
-                                            'error')
+                        log_error('Unable to connect to addic7ed.com')
+                        dialog.notification(get_ui_string(32002),
+                                            get_ui_string(32005), 'error')
                         return
                     except SubsSearchError:
-                        log('No subs found.', xbmc.LOGNOTICE)
+                        log_notice('No subs found.')
                         return
                 else:
-                    log('Episode selection cancelled.', xbmc.LOGNOTICE)
+                    log_notice('Episode selection cancelled.')
                     return
-            log('Found subs for "{0}"'.format(query), xbmc.LOGNOTICE)
+            log_notice('Found subs for "{0}"'.format(query))
             display_subs(results.subtitles, results.episode_url, filename)
 
 
@@ -225,5 +215,7 @@ def router(paramstring):
         # Search and display subs.
         search_subs(params)
     elif params['action'] == 'download':
-        download_subs(params['link'], params['ref'], urllib.unquote_plus(params['filename']))
-    xbmcplugin.endOfDirectory(_handle)
+        download_subs(
+            params['link'], params['ref'], urllib.unquote_plus(params['filename'])
+        )
+    xbmcplugin.endOfDirectory(handle)
