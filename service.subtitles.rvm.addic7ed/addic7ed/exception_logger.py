14942e5ab9588ac1e21191a3b35f49ba1922ec18
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2020-09-28 14:52:23 +0300

    Add __future__ import to exception_logger.py

--- service.subtitles.rvm.addic7ed/addic7ed/exception_logger.py
+++ service.subtitles.rvm.addic7ed/addic7ed/exception_logger.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """Exception logger with extended diagnostic info"""
+from __future__ import absolute_import, unicode_literals
 
 import inspect
 import sys
