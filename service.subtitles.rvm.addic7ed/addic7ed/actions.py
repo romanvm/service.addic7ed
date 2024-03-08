e6ddbb1c54cbf019ca20ae174941df9c5706904a
Roman <roman1972@gmail.com>
2024-03-09 01:40:59 +0200

    Add a debug message

--- service.subtitles.rvm.addic7ed/addic7ed/actions.py
+++ service.subtitles.rvm.addic7ed/addic7ed/actions.py
@@ -174,6 +174,7 @@ def extract_episode_data():
     :raises ParseError: if cannot determine episode data
     """
     now_played = get_now_played()
+    logger.debug(f'Played file info: {now_played}')
     showname = now_played['showtitle'] or xbmc.getInfoLabel("VideoPlayer.TVshowtitle")
     parsed = urlparse.urlparse(now_played['file'])
     filename = os.path.basename(parsed.path)
