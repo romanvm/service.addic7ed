e2d5fc35181e7096a780b421a58e7175b991625a
Roman <roman1972@gmail.com>
2024-03-09 01:03:31 +0200

    Update webclient user agent

--- service.subtitles.rvm.addic7ed/addic7ed/webclient.py
+++ service.subtitles.rvm.addic7ed/addic7ed/webclient.py
@@ -22,7 +22,7 @@ __all__ = ['Session']
 SITE = 'https://www.addic7ed.com'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-                  '(KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+                  '(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Host': SITE[8:],
     'Accept-Charset': 'UTF-8',
