1db1d4b8f9b22d2a45137af8b46809d38d7d1e9f
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2024-06-23 13:35:50 +0300

    Fix a crash on addon initialization

--- service.subtitles.rvm.addic7ed/addic7ed/webclient.py
+++ service.subtitles.rvm.addic7ed/addic7ed/webclient.py
@@ -42,7 +42,7 @@ class Session:
 
     def __new__(cls):
         if cls._instance is None:
-            cls._instance = super().__new__()
+            cls._instance = super().__new__(cls)
         return cls._instance
 
     def __init__(self):
