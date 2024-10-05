ba07b0d202ea73fd036187bb83786f93a88cf0df
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2024-10-05 15:18:47 +0300

    catch LookupError as well

--- service.subtitles.rvm.addic7ed/addic7ed/simple_requests.py
+++ service.subtitles.rvm.addic7ed/addic7ed/simple_requests.py
@@ -124,7 +124,7 @@ class Response:
         if self._text is None:
             try:
                 self._text = self.content.decode(self.encoding)
-            except UnicodeDecodeError:
+            except (UnicodeDecodeError, LookupError):
                 self._text = self.content.decode('utf-8', 'replace')
         return self._text
 
