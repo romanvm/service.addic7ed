9e00ba7267ffa8530d797c85eadb35a3a4cc0a84
Roman Miroshnychenko <roman.miroshnychenko@comeet.co>
2024-12-14 14:10:28 +0200

    Update user-agent string

--- service.subtitles.rvm.addic7ed/addic7ed/webclient.py
+++ service.subtitles.rvm.addic7ed/addic7ed/webclient.py
@@ -28,7 +28,7 @@ logger = logging.getLogger(__name__)
 SITE = 'https://www.addic7ed.com'
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
-                  '(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+                  '(KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Host': SITE[8:],
     'Accept-Charset': 'UTF-8',
